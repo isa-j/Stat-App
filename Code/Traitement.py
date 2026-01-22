dc19104edfc06c8cdc62b4aac0b3efcfc4560685
« rolandM91 » <rolandmaury05@gmail.com>
2026-01-22 03:44:17 +0100

    gg

--- Code/Traitement.py
+++ Code/Traitement.py
@@ -121,12 +121,14 @@ df_Indic = df_Indic.drop(columns=to_drop)
 df_Indic = df_Indic.dropna(subset=["OBS_VALUE"])
 
 df_Indic["year"] = df_Indic["TIME_PERIOD"].str.slice(0, 4).astype(int)
-df_Indic = df_Indic[df_Indic["year"] >= 1970]
+df_Indic = df_Indic[df_Indic["year"] >= 1988]
+
 df_Indic.to_parquet("Indicateurs.parquet")
 
 #df_Indic.to_csv("/Users/roland/Desktop/ENSAE 2A/Statapp/Github/Stat-App/Data_clean/Indicateurs macro.csv")
 
 
+import pandas as pd
 
-
-
+df = pd.read_parquet("/Users/roland/Desktop/ENSAE 2A/Statapp/Github/Stat-App/Data_clean/Indicateurs.parquet")
+df["year"].unique()
