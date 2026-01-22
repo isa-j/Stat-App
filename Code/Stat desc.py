fc895eae88834c286994c799ad0d50d16b390bbd
« rolandM91 » <rolandmaury05@gmail.com>
2026-01-22 18:46:50 +0100

    Premiers graphiques

--- Code/Stat desc.py
+++ Code/Stat desc.py
@@ -49,5 +49,50 @@ df_final = df_indic.merge(
 )
 
 #df_final.to_parquet("/Users/roland/Desktop/ENSAE 2A/Statapp/Github/Stat-App/Data_clean/Indicators and tarifs.parquet")
+df_final = df_final[df_final["Fréquence d'observation"] == "Annuelle"]
+df_final
 
 
+
+
+#Il y a des lignes identiques qui servent à rien dans le df, on va vérifier qu'elles sont bien identiques partout
+# On filtre les lignes correspondant à chaque transformation
+df_période = df_final[df_final['Transformation'] == 'Taux de croissance, période sur période']
+df_sur_an = df_final[df_final['Transformation'] == 'Taux de croissance, sur un an']
+
+# On s'assure qu'elles sont bien alignées par exemple par une colonne 'Année' ou 'Date'
+df_merged = df_période.merge(df_sur_an, on='year', suffixes=('_période', '_sur_an'))
+
+# On compare les valeurs
+toutes_identiques = (df_merged['OBS_VALUE_période'].values == df_merged['OBS_VALUE_sur_an'].values).all()
+
+print(toutes_identiques) #C'est bon on avait raison c identique
+
+
+
+df_final = df_final[df_final['Transformation'] != 'Taux de croissance, période sur période']
+df_final.to_parquet("/Users/roland/Desktop/ENSAE 2A/Statapp/Github/Stat-App/Data_clean/Indicators and tarifs V2")
+
+
+df_france = df_final[(df_final["Country Code"] == "FRA") & (df_final["tariff_type"] == "AR")]
+df_france = df_france[ (df_france["Mesure"] == "Production volume") & (df_france["Activité économique"] == "Industrie (sauf construction)") &(df_france["Unité de mesure"] == "Taux de croissance")]
+
+
+####Graphique de corrélation (PB: mieux vaudrait utiliser le lag des tarifs pour capter l'effet des politiques protectionnistes)
+
+#Version "naïve"
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Scatter plot simple avec seaborn
+plt.figure(figsize=(8,5))
+sns.scatterplot(data=df_france, x='tariff', y='OBS_VALUE')
+
+# Ajouter une droite de régression pour visualiser la tendance
+sns.regplot(data=df_france, x='tariff', y='OBS_VALUE', scatter=False, color='red')
+
+plt.title("Corrélation entre tarifs douaniers et taux de croissance de la production (France, 1988-2022)")
+plt.xlabel("Tarif douanier (%)")
+plt.ylabel("Taux de croissance de la production (%)")
+plt.show()
